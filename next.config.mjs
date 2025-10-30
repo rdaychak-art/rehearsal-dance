de7cfe26b4539be549c2a0d9d5d51173987dce04
michel-adelino <micheladelino222@gmail.com>
2025-10-30 16:57:52 -0300

    eslint disable

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
